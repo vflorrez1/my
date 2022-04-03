f8b533df5bdbd88122943a2ab239fe34ff311a44
vic <manuel1212@hotmail.co.uk>
2022-04-03 14:46:26 +0100

    my first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+import { FC } from 'react';
+
+const App: FC = () => (
+  <div className="App" style={{ display: 'flex' }}>
+    <div style={{ display: 'flex', flex: 1 }}>My</div>
+  </div>
+);
+export default App;
